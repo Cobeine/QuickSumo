3b98a4ddff38d0dfe652b2ca208af92805245d11
ShellScrpt <SalahElwaleed.a@gmail.com>
2022-09-05 22:21:16 +0200

    yaml file base

---
+++ src/main/java/me/cobeine/qSumo/utils/data/YamlFile.java
@@ -0,0 +1,107 @@
+/**
+ * MIT License
+ * <p>
+ * Copyright (c) 2022 Cobeine
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE. YOU ARE NOT ALLOWED TO RE-DISTRIBUTE AND/OR REPUBLISH. YOU ARE NOT ALLOWED TO FORK
+ * UNLESS GIVEN CREDIT TO THE ORIGINAL AUTHOR (COBEINE)
+ */
+package me.cobeine.qSumo.utils.data;
+
+import com.google.common.io.ByteStreams;
+import me.cobeine.qSumo.Core;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
+public abstract class YamlFile {
+    public String path;
+    public String name;
+    private final File file;
+    private YamlConfiguration cfg;
+
+    public YamlFile(String name, String path) {
+        this.path = path;
+        this.name = name;
+        this.file = new File(path, name + ".yml");
+
+    }
+
+    public void createNew() {
+        try {
+            if (file.createNewFile()) {
+                Bukkit.getLogger().info("File '" + name + ".yml' has been created");
+                setDefaults();
+            }
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            Bukkit.getLogger().severe("Failed to create file '" + name + ".yml' please check the error above");
+
+        }
+
+        if (file.exists())
+            cfg = YamlConfiguration.loadConfiguration(file);
+    }
+
+    public void setDefaultConfig() {
+        try {
+            InputStream stream = Core.getInstance().getResource(name + ".yml");
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            ByteStreams.copy(stream, fileOutputStream);
+            stream.close();
+        } catch (Exception ignored) {
+        }
+    }
+    public abstract void setDefaults();
+
+    public boolean exists() {
+        return file.exists();
+    }
+
+    public void delete() {
+        file.delete();
+        cfg = null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void save() {
+        try {
+            cfg.save(file);
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            Bukkit.getLogger().info("Failed to save file '" + name + ".yml' please check the error above");
+
+        }
+    }
+
+
+    public YamlConfiguration getConfig() {
+        return cfg;
+    }
+}
